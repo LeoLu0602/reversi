aa11cb95b6934583d55f36e04ad6c615a9a09c28
Leo <96434243+LeoLu0602@users.noreply.github.com>
2023-09-19 16:58:34 +0800

    Handle game over

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { reset } from './features/game/gameSlice';
 import Cell from './components/Cell';
 import styles from './app.module.css';
 
 const App = () => {
+    const [gameOverMsgShown, setGameOverMsgShown] = useState(false);
+
     const turn = useSelector(state => state.game.turn);
+    const isGameEnd = useSelector(state => state.game.isGameEnd);
+
+    const dispatch = useDispatch();
 
     const table = [];
 
@@ -16,6 +23,17 @@ const App = () => {
 
         table.push(<div key={i} className={styles.row}>{row}</div>);
     }
+
+    const handleGameOver = () => {
+        setGameOverMsgShown(true);
+        alert('GAME OVER');
+        setGameOverMsgShown(false);
+        dispatch(reset());
+    };
+
+    useEffect(() => {
+        if (isGameEnd && !gameOverMsgShown) handleGameOver(); 
+    }, [isGameEnd]);
 
     return (
         <div className={styles.app}>
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { place, nextPlayer, reset } from '../features/game/gameSlice';
+import { place, nextPlayer } from '../features/game/gameSlice';
 import styles from './Cell.module.css';
 
 const Cell = ({ row, col }) => {
@@ -8,7 +8,6 @@ const Cell = ({ row, col }) => {
 
     const cellStatus = useSelector(state => state.game.board[row][col]);
     const legalMoves = useSelector(state => state.game.legalMoves);
-    const isGameEnd = useSelector(state => state.game.isGameEnd);
     
     const dispatch = useDispatch();
 
@@ -22,21 +21,11 @@ const Cell = ({ row, col }) => {
         dispatch(nextPlayer());
     };
 
-    const handleGameOver = () => {
-        dispatch(reset());
-    };
-
     useEffect(() => {
         const legalMovesSet = new Set(legalMoves);
 
         setIsLegal(legalMovesSet.has(row * 8 + col));
     }, [legalMoves]);
-
-    useEffect(() => {
-        if (!isGameEnd) return;
-
-        handleGameOver();
-    }, [isGameEnd]);
 
     return (
         <div 
