ac355813570341ce23add1ed51b82e3bb67ae06e
Leo <96434243+LeoLu0602@users.noreply.github.com>
2023-09-20 16:06:43 +0800

    Show who wins

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,10 +1,19 @@
+import { useSelector } from 'react-redux';
 import styles from './GameOver.module.css';
 
 const GameOver = ({ restart }) => {
+    const [blackScore, whiteScore] = useSelector(state => state.game.scores);
+
     return (
         <div className={styles.modal}>
             <div className={styles.box}>
-                <div className={styles.result}>White Wins</div>
+                {
+                    blackScore > whiteScore
+                    ? <div className={styles.result}>Black Wins</div>
+                    : whiteScore > blackScore 
+                    ? <div className={styles.result}>White Wins</div>
+                    : <div className={styles.result}>Draw</div>
+                }
                 <button onClick={restart}>Restart</button>
             </div>
         </div>
